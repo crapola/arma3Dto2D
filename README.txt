510272178537a38219113889feb5b82cff4c52e8
crapola <wally8086@gmail.com>
2013-01-23 22:05:44 +0100

    cleanup

---
+++ README.txt
@@ -0,0 +1,10 @@
+Arma3Dto2D
+==========
+
+A quick script to convert empty vehicles/objects from the Arma 2 3D editor to a 2D editor format.
+Objects in the air will get a setPos in their init to set the height.
+Run the script with your mission.sqf in its root and output.txt will be generated.
+You will need to paste the result in an existing .sqm of same island under the "class Vehicles" section and make sure items are properly numbered, and that the value for "items=" is correct.
+
+
+
--- arma3Dconverter.py
+++ arma3Dconverter.py
@@ -1,30 +1,33 @@
 #-------------------------------------------------------------------------------
 # Name:			arma3Dconverter
-# Purpose:		Tool for Arma 2 mission editing.
-#				Converts empty vehicles.objects to 2D editor format. Vehicles in
-#				the air get a setpos in their init.
-# Usage:		Copy your mission.sqf from 3D editor where the script is, then
-#				run it. It will generate a list of items to output.txt that you
-#				can	paste in your .sqm under Vehicles. Pay attention to proper
-#				item count and numbering etc.
+# Purpose:		Converts empty vehicles to 2D editor format classes.
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
-import re
+# Settings:
+
+# Items are named Item0 to ItemN. This sets the starting value.
+STARTING_ITEM_NUMBER=0
+# Height will be rounded to zero when below this.
+Z_THRESOLD=0.1
+# Round to 32ths, can disable if you want.
+ROUND_VALUES=True
+
+#-------------------------------------------------------------------------------
 
 RX_FLOAT=r"[-+]?(?:(?: \d* \. \d+ )|(?: \d+ \.? ))(?: [Ee] [+-]? \d+ ) ?"
+RX_OBJ=(
+'createVehicle \[\"(?P<name>[^"]+)' # Name
+'.+?\[(?P<pos>.+?)\], ' # Position
+'.+?(?:\}|setDir (?P<dir>[-]?\d*.\d*))' # Direction
+)
 
-Z_THRESOLD=0.1 # Below this, the object is on the ground
-SETTING_DOROUND=True
-STARTING_ITEM_NUMBER=0
+import re
 
-def roundo(value):
-	"""Nearest multiple of 1/16"""
-	if SETTING_DOROUND==False: return float(value)
-	return (1/16)*round(float(value)/(1/16))
+def roundto32(value):
+	return (1/32)*round(float(value)/(1/32))
 
 class A2Object:
-
 	item_output_format="""		class Item{}
 		{{
 			position[]={{{},{},{}}};
@@ -35,7 +38,6 @@ class A2Object:
 			vehicle="{}";
 		}};
 """
-
 	item_output_format_with_init="""		class Item{}
 		{{
 			position[]={{{},{},{}}};
@@ -44,7 +46,7 @@ class A2Object:
 			id={};
 			side="EMPTY";
 			vehicle="{}";
-			init="this setPos [{},{},{}]";
+			init="this setPos [getPos this select 0,getPos this select 1,{}]";
 		}};
 """
 	def __init__(self):
@@ -65,24 +67,21 @@ class A2Object:
 			self.name)
 		else:
 			r=self.item_output_format_with_init.format(item_number,
-										self.pos[0],
-										self.pos[2],
-										self.pos[1],
-										self.dir,
-										1000+item_number,
-										self.name,
-                                        self.pos[0],
-										self.pos[1],
-										self.pos[2])
+			self.pos[0],
+			self.pos[2],
+			self.pos[1],
+			self.dir,
+			1000+item_number,
+			self.name,
+			self.pos[2])
 		return r
 
-RX_OBJ=(
-'createVehicle \[\"(?P<name>[^"]+)' # Name
-'.+?\[(?P<pos>.+?)\], ' # Position
-'.+?(?:\}|setDir (?P<dir>[-]?\d*.\d*))' # Direction
-)
-
 def main():
+	if ROUND_VALUES:
+		roundo=roundto32
+	else:
+		roundo=float
+
 	rx=re.compile(RX_OBJ,re.S)
 	file=open('mission.sqf','r')
 	text=file.read()
@@ -93,7 +92,6 @@ def main():
 	objects_list=[]
 	for z in sections:
 		o=A2Object()
-
 		o.name=z[0]
 		postxt=rx.findall(z[1])
 		o.pos[0]=roundo(postxt[0])
@@ -101,12 +99,10 @@ def main():
 		if len(postxt)>2:
 			o.pos[2]=roundo(postxt[2])
 			if o.pos[2]<Z_THRESOLD: o.pos[2]=0
-
 		if z[2]!='':
 			o.dir=roundo(z[2])
 		else:
 			o.dir=0
-
 		objects_list.append(o)
 
 	print (len(sections),"items.")
